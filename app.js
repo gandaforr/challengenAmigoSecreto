a2fd4994db48439f5759c8bb654514d825209d47
gandaforr <131477439+gandaforr@users.noreply.github.com>
2025-08-16 16:40:48 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,59 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigo_secreto = "";
+let amigos = [];
+
+function existeAmigo(nombre){
+    if(nombre.trim() === ""){
+        return 0;
+    }
+
+    for(var i=0; i<amigos.length; i++){
+        if (amigos[i].trim().toLowerCase() == nombre.trim().toLowerCase()){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+function agregarAmigoSecret(nombre){
+    if(existeAmigo(nombre) == 0){
+        alert("los nombres debeb de ser diferentes y no pueden estar vacios");
+    }
+    else{
+        amigos.push(nombre);
+    }
+}
+
+function actualizarListaDeAmigos(){
+    //let listaAmigos = document.querySelector('#listaAmigos');
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = "";
+    for(var i = 0; i < amigos.length; i++){
+        let integranteAmigo = document.createElement("li");
+        integranteAmigo.textContent = amigos[i];
+        listaAmigos.appendChild(integranteAmigo);
+    }
+}
+
+function agregarAmigo(){
+    let amigo = document.querySelector('#amigo');
+
+    agregarAmigoSecret(amigo.value);
+    amigo.value = " ";
+    actualizarListaDeAmigos();
+}
+
+function aleotorioMatriz(matriz){
+    return matriz[Math.floor(Math.random() * matriz.length)];
+    //return Math.floor(Math.random() * n) + 1;
+}
+
+function sortearAmigo(){
+    // elegir aleatoriamente un elemento de un array y 
+    //darle ese valor a amigo secreto
+    amigo_secreto = aleotorioMatriz(amigos);
+  
+    // actualizarlo en la pagina
+    let resultado = document.querySelector('#resultado');
+    resultado.innerHTML = amigo_secreto;
+}
